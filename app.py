338e8142130817aac26062b7384ee37946ac9e78
spencerg11 <117400405+spencerg11@users.noreply.github.com>
2024-04-07 12:09:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,48 @@
-from flask import Flask
+from flask import Flask, request, redirect
+from processing import calculate_bmi, get_bmi_category
 
-app = flask(__name__)
+app = Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route('/')
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=["GET", "POST"])
+def calculate_page():
+    errors = ""
+    result = ""
+    if request.method == "POST":
+        weight = request.form["Weight_in_Lbs"]
+        feet = request.form["Height_in_Feet"]
+        inches = request.form["Inches"]
+
+        if not weight.isnumeric():
+            errors += "<p>Weight must be a number.</p>\n"
+        elif not feet.isnumeric():
+            errors += "<p>Height in feet must be a number.</p>\n"
+        elif not inches.isnumeric():
+            errors += "<p>Inches must be a number.</p>\n"
+        else:
+            weight = float(weight)
+            feet = float(feet)
+            inches = float(inches)
+            bmi = calculate_bmi(feet, inches, weight)
+            category = get_bmi_category(bmi)
+            result = f'<p>Your BMI is {bmi:.2f}, which falls into the category of {category}</p>'
+
+    return '''
+        <html>
+            <body>
+                <p> Enter the following information:</p>
+                <form method="post" action=".">
+                    <p><label for="Weight_in_Lbs">Weight in Lbs:</label> <input type="text" name="Weight_in_Lbs" id="Weight_in_Lbs"></p>
+                    <p><label for="Height_in_Feet">Height in Feet:</label> <input type="text" name="Height_in_Feet" id="Height_in_Feet"></p>
+                    <p><label for="Inches">Inches:</label> <input type="text" name="Inches" id="Inches"></p>
+                    <p><input type="submit" value="Calculate BMI" /></p>
+                </form>
+                {result}
+                {errors}
+            </body>
+        </html>
+    '''.format(result=result, errors=errors)
+
+if __name__ == "__main__":
+    app.run()
 
